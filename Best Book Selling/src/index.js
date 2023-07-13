8c05ce8e20c694233a0a7bb03a6b5e582fc26b3d
Ritik Kumar Singh <ritiksingh0523@gmail.com>
2023-07-13 10:55:22 +0530

    Best Book Selling

---
+++ Best Book Selling/src/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css"
+import { bookList } from "./books"
+import Book from "./Book"
+
+
+
+
+
+
+
+
+
+const BookList = () => {
+    return (
+        <>
+            <h1>Amazon Best Seller</h1>
+            <section className="booklist">
+                {bookList.map((books, index) => {
+                    return <Book {...books} key={books.id} number={index} />
+                })
+
+                }
+            </section>
+        </>
+    );
+}
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<BookList />);
